f74a9573535546e6ac980ce8b49ae7ee7bfba779
armando baltazar <armandobaltazarmx@gmail.com>
2023-11-30 14:13:32 -0600

    Add project files.

---
+++ BeneficiarioAPI/Controllers/BeneficiarioController.cs
@@ -0,0 +1,71 @@
+﻿using BeneficiarioAPI.Data;
+using BeneficiarioAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BeneficiarioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeneficiarioController : ControllerBase
+    {
+
+        [HttpGet("ListaBeneficiarios{idEmpleado}")]
+        public IEnumerable<Beneficiario> GetBeneficiarios(int idEmpleado)
+        {
+            return BeneficiarioStore.BeneficiarioLista(idEmpleado);
+        }
+
+
+        [HttpGet("SelecionaBeneficiario{id}")]
+        public Beneficiario GetBeneficiario(int id)
+        {
+            return BeneficiarioStore.SeleccionaBeneficiario(id);
+        }
+
+        [HttpGet("TotalPorcentajes{idEmpleado}")]
+        public int GetTotalPorcentaje(int idEmpleado)
+        {
+            int Res;
+            Res = BeneficiarioStore.TotalPorcentaje(idEmpleado);
+            return Res;
+        }
+
+        [HttpGet("TotalPorcentajesMenosBeneficiario{idEmpleado}/{idBeneficiario}")]
+        public int GetTotalPorcentajeMenosBeneficiario(int idEmpleado, int idBeneficiario)
+        {
+            int Res;
+            Res = BeneficiarioStore.TotalPorcentajeMenosBeneficiario(idEmpleado, idBeneficiario);
+            return Res;
+        }
+
+        // POST api/<BeneficiarioController>
+        [HttpPost("AgregaBeneficiario")]
+        public bool AgregaBeneficiario(Beneficiario reg)
+        {
+            return BeneficiarioStore.BeneficiarioAgregar(reg);
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost("ActualizaBeneficiario")]
+        public bool ActualizaBeneficiario(Beneficiario reg)
+        {
+            return BeneficiarioStore.BeneficiarioActualiza(reg);
+        }
+        // PUT api/<BeneficiarioController>/5
+        [HttpPut("PutBeneficiario{id}")]
+        public void PutBeneficiario(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BeneficiarioController>/5
+        [HttpDelete("BorraBeneficiario{id}")]
+        public bool DeleteBeneficiario(int id)
+        {
+            return BeneficiarioStore.BeneficiarioBorrar(id);
+        }
+
+
+    }
+
+}
---
+++ BeneficiarioAPI/Data/beneficiariosData.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+
+namespace BeneficiarioAPI.Data
+{
+    public class beneficiariosData
+    {
+        SqlConnection cnstr = new SqlConnection(@"server=XXXXXX\SQLEXPRESS; database=dbBeneficiarios; user id=XXXsaXXX; password=XXXXXX; Trusted_Connection= True; MultipleActiveResultSets= True;TrustServerCertificate= true;Encrypt= true");
+        public SqlConnection getConnection
+        {
+            get { return cnstr; }
+        }
+    }
+}
